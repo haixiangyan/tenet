62228e24d63dd2677e6c5dbfcd11b1e81cf38bd2
tx-haixiang <haixiangyan@tencent.com>
2020-09-06 13:14:36 +0800

    feat: add enable and disable event

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,23 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+const IS_INVERSION = 'is_inversion';
+
 export function activate(context: vscode.ExtensionContext) {
+	console.log('Welcome to use tenet!');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "tenet" is now active!');
+	const enableInversion = vscode.commands.registerCommand('tenet.enableInversion', () => {
+		context.globalState.update(IS_INVERSION, true);
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('tenet.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from tenet!');
+		vscode.window.showInformationMessage('Entering the inversion world, stay safe!');
 	});
 
-	context.subscriptions.push(disposable);
+	const disableInversion = vscode.commands.registerCommand('tenet.disableInversion', () => {
+		context.globalState.update(IS_INVERSION, false);
+
+		vscode.window.showInformationMessage('Leaving the inversion world, goodbye!');
+	});
+
+	context.subscriptions.push(enableInversion, disableInversion);
 }
 
 // this method is called when your extension is deactivated
