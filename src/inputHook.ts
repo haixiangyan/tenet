f99cf52d74d50051a7a3afea4bffac01e5a1ede1
tx-haixiang <haixiangyan@tencent.com>
2020-09-06 14:48:27 +0800

    feat: add inverted text

--- src/inputHook.ts
+++ src/inputHook.ts
@@ -1,18 +1,38 @@
 import * as vscode from 'vscode';
+import { TextDocumentContentChangeEvent } from 'vscode';
 
-const firstPosition = new vscode.Position(0, 0);
+let isEditing = false;
+
+const getWholeRange = (document: vscode.TextDocument): vscode.Range => {
+    const firstLine = document.lineAt(0);
+    const lastLine = document.lineAt(document.lineCount - 1);
+    return new vscode.Range(firstLine.range.start, lastLine.range.end);
+};
+
+const getInvertedText = (text: string): string => {
+    return text[text.length - 1] + text.slice(0, text.length - 1);
+};
+
+const isEmpty = (changes: ReadonlyArray<TextDocumentContentChangeEvent>): boolean => {
+    return !changes || changes.some(c => !c.text)
+};
 
 const initInputHook = () => {
     vscode.workspace.onDidChangeTextDocument(event => {
-        event.contentChanges.forEach(change => {
-            if (change.text.length > 30) {
-                return;
-            }
+        if (isEditing) {return;}
+        if (isEmpty(event.contentChanges)) {return;}
 
-            const newChar = change.text;
-            
-            vscode.TextEdit.insert(firstPosition, newChar);
-        });
+        const editor = vscode.window.activeTextEditor;
+
+        if (!editor) {return;}
+
+        const invertedText = getInvertedText(event.document.getText());
+        const wholeRange = getWholeRange(event.document);
+
+        editor.edit(builder => {
+            isEditing = true;
+            builder.replace(wholeRange, invertedText);
+        }).then(() => isEditing = false);
     });
 };
 
